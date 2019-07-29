d323904b8386bfcb5eb4540354231415e702ab57
kejace <kejace@gmail.com>
2019-07-29 16:27:38 -0400

    Got blinky to work on icoboard

--- blinky/src/Top.hs
+++ blinky/src/Top.hs
@@ -15,23 +15,21 @@ import Control.Lens as Lens (Lens, Lens', over)
     { t_name     = "blinky"
     , t_inputs   =
         [ PortName "clk"
-        , PortName "pmod1"
-        , PortName "pmod2"
-        , PortName "pmod3"
-        , PortName "pmod4"
+        , PortName "pmod1_1"
+        , PortName "pmod1_2"
+        , PortName "pmod1_3"
+        , PortName "pmod1_4"
         ]
     , t_output   = PortProduct "out"
         [ PortName "led1"
         , PortName "led2"
         , PortName "led3"
-        , PortName "led4"
-        , PortName "led5"
         ]
     }) #-}
 topEntity
   :: Clock System Source
   -> Bit -> Bit -> Bit
-  -> Signal System (Bit, Bit, Bit, Bit, Bit)
+  -> Signal System (Bit, Bit, Bit)
 topEntity clk _ _ _ =
   withClockReset clk (unsafeToSyncReset (pure False)) $
     leds <$> counter
@@ -43,8 +41,6 @@ topEntity clk _ _ _ =
       ( c!24
       , c!23
       , c!22
-      , c!21
-      , c!20
       )
 
 main :: IO ()
